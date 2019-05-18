c2f9b041b0ca5759279a32ec3752216420ebb58e
cepthomas <cepthomas@gmail.com>
2019-05-17 19:25:44 -0500

    Main part done.

--- ct1.js
+++ ct1.js
@@ -1,14 +1,2 @@
 
-
-// The script.js file is pretty straighforward. When the runBtn button is clicked, it gets the text of the textarea,
-// passes it through showdown that converts the markdown into html. The output is then put inside the div
-// (replacing the previous content).
-
-// function run() {
-//   var text = document.getElementById('sourceTA').value,
-//       target = document.getElementById('targetDiv'),
-//       converter = new showdown.Converter(),
-//       html = converter.makeHtml(text);
-    
-//     target.innerHTML = html;
-// }
+// Nothing here...
